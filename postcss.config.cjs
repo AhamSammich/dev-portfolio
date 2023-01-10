285e6b1977c45e89f2a9a815182a22292fbc8a1d
AhamSammich <kuro.to.red@gmail.com>
2023-01-09 23:06:02 -1000

    Initial commit for MPA

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+const autoprefixer = require("autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+
+module.exports = {
+    plugins: [
+        autoprefixer({
+            
+        }),
+        postcssPresetEnv({
+            stage: 1,
+        })
+    ]
+}
