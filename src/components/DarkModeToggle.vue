d2b0e0a06cdcfef7474a10f9ebe050f691d6a35e
AhamSammich <kuro.to.red@gmail.com>
2023-01-11 01:02:31 -1000

    Updated history page with images; added Discord to socials; added color scheme preference

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+// const toggleColorScheme = (input) => {
+//     if
+// }
+
+</script>
+
+<template>
+  <fieldset>
+    <div>
+      <label for="toggle-dark"><slot name="enable-dark-mode"></slot></label>
+      <!-- <input type="radio" name="color-scheme" id="toggle-dark" :onchange=toggleColorScheme({e}) /> -->
+    </div>
+    <div>
+      <label for="toggle-light"><slot name="disable-dark-mode"></slot></label>
+      <input type="radio" name="color-scheme" id="toggle-light" onchange="toggleColorScheme" />
+    </div>
+  </fieldset>
+</template>
+
+<style scoped>
+fieldset {
+  border: none;
+  padding: 0.25em;
+  position: absolute;
+  display: flex;
+  gap: 0.25em;
+  top: 3em;
+  left: 85%;
+  z-index: 5;
+}
+
+input {
+  appearance: none;
+}
+
+label {
+  cursor: pointer;
+  z-index: 3;
+}
+
+label:has(~ input[checked]) {
+  display: none;
+}
+
+</style>
