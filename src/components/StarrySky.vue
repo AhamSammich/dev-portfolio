6e7e020be132dd84dd40eba99f21dfe92c94c26e
AhamSammich <kuro.to.red@gmail.com>
2023-01-16 12:53:04 -1000

    Improved responsiveness

--- src/components/StarrySky.vue
+++ src/components/StarrySky.vue
@@ -13,7 +13,7 @@ function checkSuccess(rate: number): boolean {
 
 function randomColor(): string {
   let color = checkSuccess(50) ? "lightblue" : "lightpink";
-  return checkSuccess(20) ? color : "white";
+  return checkSuccess(20) ? color : "lightgoldenrodyellow";
 }
 
 function randomSize(): number {
