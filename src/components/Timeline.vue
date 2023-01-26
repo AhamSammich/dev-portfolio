f46803d2503834ffb353b1e76aa0708af98600ff
AhamSammich <kuro.to.red@gmail.com>
2023-01-26 10:45:18 -1000

    Create timeline component and route

---
+++ src/components/Timeline.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps<{
+  events: {
+    name: string;
+    year: number;
+    description: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <div id="timeline">
+    <div id="dateline">
+      <template v-for="ev in events">
+        <div class="event-card">
+          <h1>{{ ev.year }}</h1>
+          <h2>{{ ev.name }}</h2>
+          <p>{{ ev.description }}</p>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+#dateline {
+  height: 100dvh;
+  overflow-x: scroll;
+  overflow-y: hidden;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  &::-webkit-scrollbar {
+    height: 0.3rem;
+  }
+}
+
+.event-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightcyan;
+  margin: 3rem;
+  padding: 1rem;
+}
+</style>
